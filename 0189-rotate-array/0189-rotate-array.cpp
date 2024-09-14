732bdae1fa35662f1d2107c0cb375afbb731fce8
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-09-14 16:47:01 +0600

    Time: 22 ms (43.53%), Space: 27.5 MB (92.7%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-
-// This got time limit error
-    void rotate(vector<int>& nums, int k) {
-        for (auto i = 0; i < k; i++) {
-            int lastElement = nums[nums.size() - 1];
-            nums.pop_back();
-            nums.insert(nums.begin(), lastElement);
-        }
+    void rotate(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        if (k == 0)
+            return;
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + k);
+        std::reverse(nums.begin() + k, nums.end());
     }
 };
