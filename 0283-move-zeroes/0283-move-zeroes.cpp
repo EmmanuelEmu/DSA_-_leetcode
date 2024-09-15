60fbd81cefc87066e3c929adfa0cbf9ba50a9451
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-09-15 23:49:42 +0600

    Time: 3 ms (99.94%), Space: 21.9 MB (75.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    // Optimal Solution
     void moveZeroes(vector<int>& nums) {
-    int nonZeroPos = 0;
+        int n = nums.size();
+        int zeroInd = -1;
+        for (auto i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                zeroInd = i;
+                break;
+            }
+        }
 
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            nums[nonZeroPos] = nums[i];
-            nonZeroPos++;
+        if (zeroInd != -1) {
+            for (auto i = zeroInd + 1; i < n; i++) {
+                if (nums[i] != 0) {
+                    swap(nums[i], nums[zeroInd]);
+                    zeroInd++;
+                }
+            }
         }
     }
-
-    for (int i = nonZeroPos; i < nums.size(); i++) {
-        nums[i] = 0;
-    }
-}
-
 };
