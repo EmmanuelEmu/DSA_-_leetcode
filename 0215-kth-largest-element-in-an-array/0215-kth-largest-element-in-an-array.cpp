55f8bc1c9918db4131bc00e7cf7e859f297c74ac
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-09-20 14:06:57 +0600

    Time: 160 ms (5.99%), Space: 70.6 MB (11.46%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()-k];
+        map<int, int> mp;
+        for (auto it : nums) {
+            mp[it]++;
+        }
+
+        // map is internally sorted as always
+        // sort(mp.begin(), mp.end());
+        for (auto it = mp.rbegin(); it != mp.rend(); it++) {
+            k -= it->second;
+            if (k <= 0) {
+                return it->first;
+            }
+        }
+
+        return -1;
     }
 };
