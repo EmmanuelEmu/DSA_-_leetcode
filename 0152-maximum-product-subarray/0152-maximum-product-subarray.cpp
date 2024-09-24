7e56a10ccdb96b87b8d08fab46fba64e777f1ab0
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-09-25 00:23:11 +0600

    Time: 5 ms (60.39%), Space: 16.7 MB (44.43%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int res = INT_MIN;
+        for (auto i = 0; i < nums.size(); i++) {
+            if (prefix == 0) {
+                prefix = 1;
+            }
+            if (suffix == 0) {
+                suffix = 1;
+            }
+            prefix *= nums[i];
+            suffix *= nums[nums.size() - i - 1];
+            res = max(res, max(prefix, suffix));
+        }
+        return res;
+    }
+};
