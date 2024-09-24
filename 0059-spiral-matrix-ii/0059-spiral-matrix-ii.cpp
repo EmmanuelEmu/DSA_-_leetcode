b607ab6902b990dcac136075b565a61657bca38a
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-09-24 19:57:22 +0600

    Time: 0 ms (100%), Space: 8 MB (75.23%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int row = n;
+        int col = n;
+        int val = 1;
+        int left = 0, right = col - 1, top = 0, bottom = row - 1;
+        vector<vector<int>> matrix(row, vector<int>(col));
+
+        while (left <= right && top <= bottom) {
+            for (auto i = left; i <= right; i++) {
+                matrix[top][i] = val++;
+            }
+            top++;
+            for (auto i = top; i <= bottom; i++) {
+                matrix[i][right] = val++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (auto i = right; i >= left; i--) {
+                    matrix[bottom][i] = val++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (auto i = bottom; i >= top; i--) {
+                    matrix[i][left] = val++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+};
