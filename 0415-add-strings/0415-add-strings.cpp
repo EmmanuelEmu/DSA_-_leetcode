28c2b5330ad2729b76cc4f3318d67d7ad57501b6
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-02 21:29:29 +0600

    Time: 4 ms (52.5%), Space: 10.8 MB (24.25%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string res = "";
+        int carry = 0;
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+
+        string bigNumber = num1.size() >= num2.size() ? num1 : num2;
+        string smallNumber = num1.size() < num2.size() ? num1 : num2;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2[j] - '0';
+                j--;
+            }
+            res += to_string(sum % 10);
+            carry = sum / 10;
+        }
+
+        res = string(res.rbegin(), res.rend());
+        return res;
+    }
+};
