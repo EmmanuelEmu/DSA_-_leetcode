47ab88e4de6c40f8790434bc26ddb512ebc92da8
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-02 20:39:57 +0600

    Time: 61 ms (91.27%), Space: 12 MB (47.21%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isPalindrome(const string& s, int start, int end) {
+        while (start <= end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n, INT_MAX);
+
+        // Precompute palindrome information in a 2D array
+        vector<vector<bool>> isPalin(n, vector<bool>(n, false));
+
+        // Precompute all substrings to check if they are palindromes
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+                if (s[start] == s[end] &&
+                    (end - start <= 1 || isPalin[start + 1][end - 1])) {
+                    isPalin[start][end] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (isPalin[0][i]) {
+                dp[i] = 0;
+            } else {
+                for (int j = 0; j < i; j++) {
+                    if (isPalin[j + 1][i]) {
+                        dp[i] = min(
+                            dp[i],
+                            dp[j] +
+                                1); // Add one cut if s[j+1...i] is a palindrome
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
