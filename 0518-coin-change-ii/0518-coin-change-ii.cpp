64ac92f145b1690ef60e33455edcec460a53c777
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-08 11:37:18 +0600

    Time: 22 ms (22.38%), Space: 27.1 MB (18.05%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int ind, vector<int>& coins, int amount,
+              vector<vector<int>>& dp) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (ind == 0) {
+            if (amount % coins[0] == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (dp[ind][amount] != -1) {
+            return dp[ind][amount];
+        }
+
+        int notTake = solve(ind - 1, coins, amount, dp);
+        int take = 0;
+        if (coins[ind] <= amount) {
+            take = solve(ind, coins, amount - coins[ind], dp);
+        }
+        return dp[ind][amount] = take + notTake;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+        return solve(coins.size() - 1, coins, amount, dp);
+    }
+};
