cbc01873cc75167941bde23e5d5251ec4e8b7e3c
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-21 19:27:17 +0600

    Time: 31 ms (29.83%), Space: 42.9 MB (27.06%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2, string& s1, string& s2,
-              vector<vector<int>>& dp) {
-        if (ind2 < 0)
-            return 1;
-        if (ind1 < 0)
-            return 0;
-        if (dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
-        if (s1[ind1] == s2[ind2]) {
-            return dp[ind1][ind2] = solve(ind1 - 1, ind2 - 1, s1, s2, dp) +
-                                    solve(ind1 - 1, ind2, s1, s2, dp);
-        } else {
-            return dp[ind1][ind2] = solve(ind1 - 1, ind2, s1, s2, dp);
-        }
-    }
-
     int numDistinct(string s, string t) {
+        // Tabulation code
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(n - 1, m - 1, s, t, dp);
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+        for (auto i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (auto j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (auto i = 1; i <= n; i++) {
+            for (auto j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 };
