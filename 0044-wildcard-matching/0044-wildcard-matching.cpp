472495b545a34035697ca38a65d8c8b611d21c41
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-22 00:08:28 +0600

    Time: 26 ms (81.55%), Space: 31.2 MB (39.61%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool solve(int ind1, int ind2, string& s, string& p,
+               vector<vector<int>>& dp) {
+        if (ind1 < 0 && ind2 < 0)
+            return true;
+        if (ind1 < 0 && ind2 >= 0)
+            return false;
+        if (ind2 < 0 && ind1 >= 0) {
+            for (int i = 0; i <= ind1; i++) {
+                if (s[i] != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (s[ind1] == p[ind2] || s[ind1] == '?') {
+            return dp[ind1][ind2] = solve(ind1 - 1, ind2 - 1, s, p, dp);
+        }
+        if (s[ind1] == '*') {
+            return dp[ind1][ind2] = solve(ind1 - 1, ind2, s, p, dp) |
+                                    solve(ind1, ind2 - 1, s, p, dp);
+        }
+        return dp[ind1][ind2] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        return solve(m - 1, n - 1, p, s, dp);
+    }
+};
