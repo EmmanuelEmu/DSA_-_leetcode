581f446d338aff63af23ab18111aec15664875ff
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-10-23 20:21:47 +0600

    Time: 462 ms (33.21%), Space: 222.2 MB (31.11%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
+    int solve(int ind, int buy, vector<int>& prices,
+              vector<vector<vector<int>>>& dp, int trans) {
+        if (ind == prices.size() || trans == 2) {
+            return 0; // No more profit can be made
+        }
+
+        if (dp[ind][buy][trans] != -1) {
+            return dp[ind][buy][trans];
+        }
+
+        int profit = 0;
+        if (buy) {
+            // Option to buy or skip
+            profit = max(-prices[ind] + solve(ind + 1, 0, prices, dp, trans),
+                         solve(ind + 1, 1, prices, dp, trans));
+        } else {
+            // Option to sell or skip
+            profit = max(prices[ind] + solve(ind + 1, 1, prices, dp, trans + 1),
+                         solve(ind + 1, 0, prices, dp, trans));
+        }
+
+        return dp[ind][buy][trans] = profit;
+    }
+
     int maxProfit(vector<int>& prices) {
-        if (prices.empty())
-            return 0;
-
         int n = prices.size();
-        vector<int> leftProfit(n, 0);
-        vector<int> rightProfit(n, 0);
-
-        int minPrice = prices[0];
-        for (int i = 1; i < n; i++) {
-            minPrice = min(minPrice, prices[i]);
-            leftProfit[i] = max(leftProfit[i - 1], prices[i] - minPrice);
-        }
-        int maxPrice = prices[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            maxPrice = max(maxPrice, prices[i]);
-            rightProfit[i] = max(rightProfit[i + 1], maxPrice - prices[i]);
-        }
-        int maxTotalProfit = 0;
-        for (int i = 0; i < n; i++) {
-            maxTotalProfit =
-                max(maxTotalProfit, leftProfit[i] + rightProfit[i]);
-        }
-
-        return maxTotalProfit;
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(2, -1)));
+        return solve(
+            0, 1, prices, dp,
+            0); // Start at index 0, buy allowed, 0 transactions completed
     }
 };
