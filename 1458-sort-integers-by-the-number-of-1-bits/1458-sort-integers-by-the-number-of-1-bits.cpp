e6c1f7afbcf1a7b56f46f712574d1b2b47ca5702
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-11-06 22:44:44 +0600

    Time: 2 ms (59.74%), Space: 13.8 MB (19.62%) - LeetHub

---
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int setBitCount(int n) {
+        int count = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    vector<int> sortByBits(vector<int>& arr) {
+        int n = arr.size();
+        map<int, vector<int>> mp;
+        for (auto i = 0; i < n; i++) {
+            mp[setBitCount(arr[i])].push_back(arr[i]);
+        }
+
+        vector<int> res;
+        for (auto it : mp) {
+            sort(it.second.begin(), it.second.end());
+            res.insert(res.end(), it.second.begin(), it.second.end());
+        }
+        return res;
+    }
+};
