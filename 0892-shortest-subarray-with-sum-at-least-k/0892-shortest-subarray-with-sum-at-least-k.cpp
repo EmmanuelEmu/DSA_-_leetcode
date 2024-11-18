eb3facd164f427eff2466067768f6aa18b7e64fb
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-11-18 19:24:11 +0600

    Time: 24 ms (93.86%), Space: 107.7 MB (73.76%) - LeetHub

---
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<long long> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+
+        int minLen = INT_MAX;
+
+        for (int i = 0; i <= n; ++i) {
+            while (!dq.empty() && prefixSum[i] - prefixSum[dq.front()] >= k) {
+                minLen = min(minLen, i - dq.front());
+                dq.pop_front();
+            }
+            while (!dq.empty() && prefixSum[i] <= prefixSum[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        return minLen == INT_MAX ? -1 : minLen;
+    }
+};
