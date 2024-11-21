e2da9a2a74e62a88e0cb983d73ae4eddce71512f
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2024-11-21 12:34:43 +0600

    Time: 2766 ms (5.16%), Space: 213.2 MB (15.03%) - LeetHub

---
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    void visitGrid(int row, int col, vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for (auto i = row + 1; i < m; i++) {
+            if (grid[i][col] == 'W' || grid[i][col] == 'G') {
+                break;
+            }
+            grid[i][col] = 'V';
+        }
+        for (auto i = row - 1; i >= 0; i--) {
+            if (grid[i][col] == 'W' || grid[i][col] == 'G') {
+                break;
+            }
+            grid[i][col] = 'V';
+        }
+        for (auto i = col + 1; i < n; i++) {
+            if (grid[row][i] == 'W' || grid[row][i] == 'G') {
+                break;
+            }
+            grid[row][i] = 'V';
+        }
+        for (auto i = col - 1; i >= 0; i--) {
+            if (grid[row][i] == 'W' || grid[row][i] == 'G') {
+                break;
+            }
+            grid[row][i] = 'V';
+        }
+    }
+
+    int countUnguarded(int m, int n, vector<vector<int>>& guards,
+                       vector<vector<int>>& walls) {
+        int count = 0;
+        vector<vector<char>> grid(m, vector<char>(n, '.'));
+
+        for (auto it : guards) {
+            grid[it[0]][it[1]] = 'G';
+        }
+
+        for (auto it : walls) {
+            grid[it[0]][it[1]] = 'W';
+        }
+
+        for (auto it : guards) {
+            int row = it[0];
+            int col = it[1];
+            visitGrid(row, col, grid);
+        }
+
+        for (auto i = 0; i < m; i++) {
+            for (auto j = 0; j < n; j++) {
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+        for (auto it : grid) {
+            for (auto i : it) {
+                if (i == '.') {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
