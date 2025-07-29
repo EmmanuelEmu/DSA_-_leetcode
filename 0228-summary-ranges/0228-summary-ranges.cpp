08aa1d9a8a3a8a45ca1cfae4a2b09ee6a78c0139
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-07-29 12:08:53 +0600

    Time: 0 ms (100%), Space: 9.4 MB (10.42%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<string> res;
+        if (n == 0)
+            return res;
+
+        int start = nums[0];
+        int end = nums[0];
+        int i = 1;
+
+        while (i < n) {
+            if (nums[i] == nums[i - 1] + 1) {
+                end = nums[i];
+                i++;
+            } else {
+                if (start == end) {
+                    res.push_back(to_string(start));
+                } else {
+                    res.push_back(to_string(start) + "->" + to_string(end));
+                }
+                start = nums[i];
+                end = nums[i];
+                i++;
+            }
+        }
+
+        // Push the last range after loop ends
+        if (start == end) {
+            res.push_back(to_string(start));
+        } else {
+            res.push_back(to_string(start) + "->" + to_string(end));
+        }
+
+        return res;
+    }
+};
