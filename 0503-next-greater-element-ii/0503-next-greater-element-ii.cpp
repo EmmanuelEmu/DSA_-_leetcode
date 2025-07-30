70bdaa9d7d12139c0a1d84f1dc3847e266f28958
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-07-31 00:49:45 +0600

    Time: 7 ms (36.02%), Space: 29.2 MB (12.83%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+        vector<int> nge;
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i % n]) {
+                st.pop();
+            }
+            if (i < n) {
+                if (st.empty())
+                    nge.push_back(-1);
+                else
+                    nge.push_back(st.top());
+            }
+            st.push(nums[i % n]);
+        }
+        reverse(nge.begin(), nge.end());
+        return nge;
+    }
+};
