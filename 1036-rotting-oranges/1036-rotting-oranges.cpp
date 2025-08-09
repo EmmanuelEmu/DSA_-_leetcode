f514026ae44342bdc4f95cbbe7e946321512c052
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-08-09 12:52:05 +0600

    Time: 1 ms (40.64%), Space: 17.2 MB (19.35%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,43 +3,50 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int, int>, int>> orangesRotting;
-        int freshOranges = 0;
+        int maxTime = 0;
+        int totalOranges = 0;
+        int rottenOranges = 0;
+
         vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>> q;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    orangesRotting.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                } else if (grid[i][j] == 1) {
-                    freshOranges++;
+                    q.push({{i, j}, 0});
+                    rottenOranges++;
+                    vis[i][j] = 1;
+                }
+                if (grid[i][j] > 0) {
+                    totalOranges++;
                 }
             }
         }
-        int time = 0;
-        int dx[4] = {-1, 0, 1, 0};
-        int dy[4] = {0, -1, 0, 1};
-        int rottenOranges = 0;
-        while (!orangesRotting.empty()) {
-            int row = orangesRotting.front().first.first;
-            int orl = orangesRotting.front().first.second;
-            time = max(time, orangesRotting.front().second);
-            orangesRotting.pop();
 
-            for (auto i = 0; i < 4; i++) {
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time = q.front().second;
+            maxTime = max(maxTime, time);
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
                 int nrow = row + dx[i];
-                int ncol = orl + dy[i];
+                int ncol = col + dy[i];
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-                    orangesRotting.push({{nrow, ncol}, time + 1});
-                    vis[nrow][ncol] = 2;
+                    vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1) {
+                    q.push({{nrow, ncol}, time + 1});
+                    vis[nrow][ncol] = 1;
+                    grid[nrow][ncol] = 2;
                     rottenOranges++;
                 }
             }
         }
 
-        if (freshOranges != rottenOranges)
-            return -1;
-        return time;
+        if (rottenOranges != totalOranges) return -1;
+        return maxTime;
     }
 };
