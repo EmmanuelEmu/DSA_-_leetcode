922ac76238737e09cb55b6f333a8fa281d9ae093
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-08-12 02:34:05 +0600

    Time: 147 ms (13.41%), Space: 148 MB (18.39%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        map<int, int> mp;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum % k == 0 && i > 0) {
+                return true;
+            }
+            int rem = sum % k;
+            if (mp.find(rem) != mp.end()) {
+                if (i - mp[rem] > 1) {
+                    return true;
+                }
+            } else {
+                mp[rem] = i;
+            }
+        }
+        return false;
+    }
+};
