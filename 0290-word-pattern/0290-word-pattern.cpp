872fd7b547bae8f684d5e6aca11a81c363a08012
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-08-16 17:05:27 +0600

    Time: 0 ms (100%), Space: 9 MB (13.34%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,51 +1,40 @@
 class Solution {
 public:
-    void removeTrailingSpaces(std::string& str) {
-        // Find the position of the last non-space character
-        size_t endPos = str.find_last_not_of(" ");
-
-        // If the string contains only spaces, erase everything
-        if (endPos == std::string::npos) {
-            str.clear();
-        } else {
-            // Erase from the last non-space character to the end
-            str.erase(endPos + 1);
-        }
-    }
-
-    bool wordPattern(string pattern, string s) {
-        unordered_set<string> wordsUsed;
+    bool isIsomorphic(string s, vector<string> t) {
         map<char, string> mp;
-        std::stringstream ss(s);
-        std::string word;
+        map<string, pair<char, bool>> used;
 
-        int i = 0;
+        if (s.size() != t.size())
+            return false;
 
-        while (ss >> word) {
-            if (i >= pattern.size()) {
-                return false;
-            }
-
-            char currentChar = pattern[i];
-
-            if (mp.count(currentChar)) {
-                if (mp[currentChar] != word) {
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.find(s[i]) != mp.end()) {
+                if (mp[s[i]] != t[i] ||
+                    (used[t[i]].second == true && used[t[i]].first != s[i]))
+                    return false;
+            } else {
+                if (used[t[i]].second == false) {
+                    mp[s[i]] = t[i];
+                    used[t[i]].first = s[i];
+                    used[t[i]].second = true;
+                } else {
                     return false;
                 }
-            } else {
-                // If the word is already used by another pattern character
-                if (wordsUsed.count(word)) {
-                    return false; // Word already mapped to another character
-                }
-                mp[currentChar] = word;
-                wordsUsed.insert(word);
             }
-            i++;
-        }
-        if (i != pattern.size()) {
-            return false;
         }
 
         return true;
+    }
+
+    bool wordPattern(string pattern, string s) {
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        if (pattern.size() != words.size())
+            return false;
+        return isIsomorphic(pattern, words);
     }
 };
