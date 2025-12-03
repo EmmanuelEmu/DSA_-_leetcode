9ac5a0fb24211c0507c4cec54da7a13e4df8c4d4
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-12-03 21:45:37 +0600

    Time: 0 ms (100%), Space: 16.8 MB (59.7%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(2*n , 0);
+        for(auto i=0 ; i<n ; i++){
+            result[i] = result[i+n] = nums[i];
+        }
+        return result;
+    }
+};
