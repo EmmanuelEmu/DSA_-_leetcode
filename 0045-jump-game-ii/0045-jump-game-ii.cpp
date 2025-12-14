6a221c59581abf894c89920038843f78dd809e1f
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-12-14 21:23:57 +0600

    Time: 0 ms (100%), Space: 20.3 MB (97.9%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int sol(vector<int>& nums, int n, int index, vector<int>& dp) {
-        if (index >= n) {
-            return 0;
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        if (nums[index] == 0) {
-            return INT_MAX;
-        }
-
-        int minVal = INT_MAX;
-        for (auto i = 1; i <= nums[index]; i++) {
-            int nextStep = sol(nums, n, index + i, dp);
-            if (nextStep != INT_MAX) {
-                minVal = min(minVal, 1 + nextStep);
-            }
-        }
-        return dp[index] = minVal;
-    }
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return sol(nums, nums.size() - 1, 0, dp);
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+        while (r < n - 1) {
+            int maxJump = 0;
+            for (int i = l; i <= r; i++) {
+                maxJump = max(maxJump, i + nums[i]);
+            }
+            l = r + 1;
+            r = maxJump;
+            jumps++;
+        }
+        return jumps;
     }
 };
