e288cce7aee1c4781f7da7c4b1fa28fe2a9d5052
Emmanuel Hembrom Emu <67520207+EmmanuelEmu@users.noreply.github.com>
2025-12-15 01:24:55 +0600

    Time: 2 ms (28.43%), Space: 9.5 MB (43.23%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        stack<char> st;
+        map<char, int> charCount;
+        map<char, bool> inStack;
+
+        for (auto chr : s) {
+            charCount[chr]++;
+        }
+        for (auto chr : s) {
+            charCount[chr]--;
+            if (!inStack[chr]) {
+                while (!st.empty() && chr < st.top() &&
+                       charCount[st.top()] > 0) {
+                    inStack[st.top()] = false;
+                    st.pop();
+                }
+                st.push(chr);
+                inStack[chr] = true;
+            }
+        }
+        string result = "";
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
